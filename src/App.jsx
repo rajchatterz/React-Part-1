1d098049cce4ac261817f3cfef75735815956a25
rajchatterz <iamraj.space@gmail.com>
2023-11-25 20:49:43 +0530

    done with the header and core component part

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import f1 from './assets/f1.png'
+import { Core_concept } from './data'
 import './App.css'
 
+function Header() {
+  const description = ['core', 'fundamental', 'Very important']
+  function getRandomChoice(max) {
+    return Math.floor(Math.random()*(max+1))
+  }
+  return (
+    <header>
+      <img src={f1}  alt="img" />
+      <h1>React Essentials</h1>
+      <p>
+        {description[getRandomChoice(2)]} React concepts you will need for almost any app you are 
+        going to build
+      </p>
+    </header>
+  )
+}
+function CoreConpect(props) {
+  return (
+    <li id='list'>
+      <img src={props.image} alt="" />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  )
+}
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header/>
+      <main>
+        <section id="core-concept">
+          {/* <h2>Core concept</h2> */}
+          <CoreConpect {...Core_concept[0]}/>
+          <CoreConpect {...Core_concept[1]}/>
+          <CoreConpect {...Core_concept[2]}/>
+          <CoreConpect {...Core_concept[3]} />
+          
+        </section>
+      </main>
     </>
   )
 }
---
+++ src/data.js
@@ -0,0 +1,31 @@
+import componentImg from './assets/component.png'
+import stateImg from './assets/state.png'
+import jsxImg from './assets/jsx.png'
+import prop from './assets/prop.png'
+
+export const Core_concept = [
+    {
+        image: componentImg,
+        title: 'Component',
+        description:
+        'The core UI building block - compose the user internface by '
+    },
+    {
+        image: stateImg,
+        title: 'state',
+        description:
+        'The core UI building block - compose the user internface by '
+    },
+    {
+        image: jsxImg,
+        title: 'JSX',
+        description:
+        'The core UI building block - compose the user internface by '
+    },
+    {
+        image: prop,
+        title: 'Prop',
+        description:
+        'The core UI building block - compose the user internface by '
+    },
+]
